5678fc589424b085d250bd653c1772dec1191b18
SashaDyts <lentrofan@gmail.com>
2022-05-24 15:48:29 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+const startBtnEl = document.querySelector('[data-start]');
+const stopBtnEl = document.querySelector('[data-stop]');
+const bodyEl = document.querySelector('body');
 
+stopBtnEl.disabled = true;
+
+startBtnEl.addEventListener('click', onStartBtnEl);
+stopBtnEl.addEventListener('click', onStopBtnEl);
+
+let timerId = null;
+
+function onStartBtnEl() {
+  bodyEl.style.backgroundColor = setBackgroundColor();
+  timerId = setInterval(setBackgroundColor, 1000);
+
+  startBtnEl.disabled = true;
+  stopBtnEl.disabled = false;
+}
+
+function onStopBtnEl() {
+  clearInterval(timerId);
+
+  stopBtnEl.disabled = true;
+  startBtnEl.disabled = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function setBackgroundColor() {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+}
